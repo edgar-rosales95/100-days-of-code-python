db3c6e795a31464bdaf4f19114721a43cd156ec5
Edgar Rosales <edgarrosales@Edgars-MBP.lan>
2025-09-06 06:23:05 -0700

    completed day1

---
+++ day01/myattempt.py
@@ -0,0 +1,4 @@
+print("Welcome tom the band name Generator!")
+city = input("What city did you grow up in? ")
+pet = input("What was the name of your first pet? ")
+print(f"Your band name is {city} {pet}")
